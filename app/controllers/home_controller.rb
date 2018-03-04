e5f56fe26bbcadab2884e73107197e6f24aef58e
Matheus Kemer <97kemer@gmail.com>
2018-03-04 17:06:54 -0300

    Render valids tickets at index and add basic format displayed data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @tickets = Ticket.all_valids
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,6 @@
 class Ticket < ApplicationRecord
+
+  def self.all_valids
+    Ticket.where("date >= ?", Time.current).order(:date)
+  end
 end
