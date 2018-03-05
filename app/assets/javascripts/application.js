5f5342b7402480e968ccf1432e81c0a05f104753
Kemer - FH <97kemer@gmail.com>
2018-03-05 12:09:24 -0300

    creating table by ajax response

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,25 @@
 function getTickets(){
   var email = $("#email").val()
   response = callAjax(email)
-  $("#responseDiv").html(response.responseText)
+  parsed_response = $.parseJSON(response.responseText)
+  parsed_response = $.parseJSON(parsed_response["product"])
+  response_finally_parsed_at_all = $.parseJSON(parsed_response)
+
+  var table = $('<table></table>');
+
+  if (response_finally_parsed_at_all == false){
+    value = "Não foi encontrado pedido para este email."
+  } else {
+    $.each(response_finally_parsed_at_all, function(i,item){
+        name = item.name
+        price = item.price
+        date = item.date
+        value = "<table class='table'><thead><tr><td>Evento</td><td>Data</td><td>Valor</td></tr></thead><tbody>"
+        value += "<tr><td>" + name + "</td><td>" + date + "</td><td>" + price + "</td></tr></tbody></table>"
+    })
+  }
+
+  $('#responseTable').html("").append(value);
 }
 
 function callAjax(email){
