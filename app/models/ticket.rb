70b98364ca4cc5c95c76ec0c7054232ae8c4d438
Kemer - FH <97kemer@gmail.com>
2018-03-05 10:33:54 -0300

    Adding validation for ticket and display flash messages

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,11 @@
 class Ticket < ApplicationRecord
   has_many :order
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :date, presence: true
+  validates :taxe, presence: true
+
   def self.all_valids
     Ticket.where("date >= ?", Time.current).order(:date)
   end
